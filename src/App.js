e5fbbca5167eb99eb8d8a6280094ee8152983a4f
Arthur Cantarela <arthur.cantarela@clickideia.com.br>
2018-06-11 16:20:36 -0300

    Start adding players

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = props => (
-  <React.Fragment>
-    <h1>Munchkin</h1>
-    <div>Level counter</div>
-  </React.Fragment>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      player: "Canta"
+    };
+  }
+
+  addPlayer = () => {
+    this.setState(prevState => ({
+      players: [...prevState.players, prevState.player],
+      player: "Canta"
+    }));
+  };
+
+  typePlayerName = e => this.setState({ player: e.target.value });
+
+  render = () => (
+    <React.Fragment>
+      <h1>Munchkin</h1>
+      <input
+        value={this.state.player}
+        onChange={this.typePlayerName.bind(this)}
+      />
+      <button onClick={this.addPlayer.bind(this)}>Add a player</button>
+      <ul>
+        {this.state.players.map((player, i) => <li key={i}>{player}</li>)}
+      </ul>
+    </React.Fragment>
+  );
+}
 
 export default App;
